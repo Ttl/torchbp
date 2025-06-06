polar_interp gradient fixes

examples/sar_process_safetensor_gpga.py
@@ -83,11 +83,8 @@ if __name__ == "__main__":
     wl = c0 / fc
     spacing = d / wl / nsweeps
     # Critically spaced array would be 0.25 wavelengths apart
-    ntheta = int(1 + nsweeps * spacing * theta_limit / 0.25)
-    nr = int((x1 - x0) / res)
-    if ffbp:
-        # Needed to avoid aliasing with ffbp
-        nr *= 2
+    ntheta = int(1 + 2 * nsweeps * spacing * theta_limit / 0.25)
+    nr = int(2 * (x1 - x0) / res)
 
     az = att[:, 2]
     mean_az = np.angle(np.mean(np.exp(1j * az)))
@@ -183,7 +180,7 @@ if __name__ == "__main__":
     tstart = time.time()
     if ffbp:
         sar_img = torchbp.ops.ffbp(fsweeps, grid_polar, fc, r_res, pos_centered,
-                stages=3, divisions=2, d0=d0)
+                stages=5, divisions=2, d0=d0)
     else:
         sar_img = torchbp.ops.backprojection_polar_2d(
             fsweeps, grid_polar, fc, r_res, pos_centered, d0)[0]

tests/test_torchbp.py
@@ -55,15 +55,14 @@ class TestPolarInterpLinear(TestCase):
         grid_polar = {"r": (10, 20), "theta": (-1, 1), "nr": 2, "ntheta": 2}
         grid_polar_new = {"r": (12, 18), "theta": (-0.8, 0.8), "nr": 3, "ntheta": 3}
         dorigin = 0.1*make_tensor((nbatch, 3), dtype=dtype)
-        # TODO: Remove this when Z-axis interpolation is supported.
-        dorigin[:,2] = 0
         args = {
             'img': make_tensor((nbatch, grid_polar["nr"], grid_polar["ntheta"]), dtype=complex_dtype),
             'dorigin': dorigin,
             'grid_polar': grid_polar,
             'fc': 6e9,
-            'rotation': 0,
+            'rotation': 0.3,
             'grid_polar_new': grid_polar_new,
+            'z0': 2
         }
         return [args]
 
@@ -98,7 +97,7 @@ class TestPolarInterpLinear(TestCase):
 
     def _test_gradients(self, device, dtype=torch.float32):
         samples = self.sample_inputs(device, requires_grad=True, dtype=dtype)
-        eps = 5e-4 if dtype == torch.float32 else 1e-4
+        eps = 1e-3 if dtype == torch.float32 else 1e-4
         rtol = 0.15 if dtype == torch.float32 else 0.05
         for args in samples:
             torch.autograd.gradcheck(